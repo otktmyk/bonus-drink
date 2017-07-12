01b7fc461ce0d14b3f66c7ef41bc8a8925bd0f76
t.ohtaka <ohtaka.tomoyuki@gmail.com>
2017-07-12 21:47:12 +0900

    answer

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,22 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    begin
+      # 負の値は、購入していないのと一緒として0を返す
+      if(amount<0)
+        return 0;
+      end
+           
+      remine = amount%3 + amount/3;
+      if(remine<3)
+        return amount + (amount/3);
+      else
+        return (amount-(amount%3)) + total_count_for(remine);
+      end
+    rescue => ex
+      # 計算失敗する事態は、呼び出し元に通知したほうがよいが
+      # 購入していないのと一緒として0を返しても問題ないだろう。
+      puts(ex.message);
+      return 0;
+    end
   end
 end
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -2,4 +2,13 @@ require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
   specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  specify { expect(BonusDrink.total_count_for(1)).to eq 1 }
+  specify { expect(BonusDrink.total_count_for(3)).to eq 4 }
+  specify { expect(BonusDrink.total_count_for(11)).to eq 16 }
+  specify { expect(BonusDrink.total_count_for(12)).to eq 17 }
+  specify { expect(BonusDrink.total_count_for(24)).to eq (24+8+2+1) }
+  specify { expect(BonusDrink.total_count_for(99)).to eq (99+33+11+3+1+1) }
+  specify { expect(BonusDrink.total_count_for(100)).to eq 149 }
+  specify { expect(BonusDrink.total_count_for(-1)).to eq 0 }
+  specify { expect(BonusDrink.total_count_for("hoge")).to eq 0 }
 end
